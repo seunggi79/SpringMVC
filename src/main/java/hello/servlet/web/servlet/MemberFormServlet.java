23e39d1c875efbaa5bde5ae3ea19d13eb1c2560c
seunggi <hsg77689469.gamil.com>
2026-01-06 15:46:40 +0900

    JSP로 회원 관리 웹 애플리케이션

--- src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberFormServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/newform")
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
 public class MemberFormServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse
